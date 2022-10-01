37f5c8e2c2e2d0d238c252df6d9381669e7d85d9
BBruington <Psychological_Chemist@hotmail.com>
2022-10-01 00:50:35 -0400

    storybook

--- apps/eldari/pages/locations/index.tsx
+++ apps/eldari/pages/locations/index.tsx
@@ -44,19 +44,19 @@ export function Index(props: IndexProps) {
               <div className="h-full py-6 px-4 sm:px-6 lg:px-8">
                 {/* Start main area*/}
                 <div className="relative h-full" style={{ minHeight: '36rem' }}>
-                <dl className="space-y-11 sm:grid sm:grid-flow-col sm:grid-cols-2 sm:grid-rows-4 sm:gap-x-6 sm:gap-y-10 sm:space-y-0 lg:gap-x-8">
-                {features.map((feature) => (
-                  <div key={feature.name} className="relative">
-                  <dt>
-                    <Link href={`/locations/${feature.name}`}>
-                      <a className='cursor-pointer ml-9 text-lg text-center font-medium leading-6 text-gray-900'>{feature.name}</a>
-                    </Link>
-                  </dt>
-                  <dd className="mt-2 ml-9 text-base text-gray-500">{feature.description}</dd>
+                  <dl className="space-y-11 sm:grid sm:grid-flow-col sm:grid-cols-2 sm:grid-rows-4 sm:gap-x-6 sm:gap-y-10 sm:space-y-0 lg:gap-x-8">
+                    {features.map((feature) => (
+                      <div key={feature.name} className="relative border-2 border-gray-200">
+                        <dt>
+                          <Link href={`/locations/${feature.name}`}>
+                            <a className='cursor-pointer ml-9 text-lg text-center font-medium leading-6 text-gray-900'>{feature.name}</a>
+                          </Link>
+                        </dt>
+                        <dd className="mt-2 ml-8 text-base text-gray-500">{feature.description}</dd>
+                      </div>
+                    ))}
+                  </dl>                
                 </div>
-                ))}
-                </dl>                
-            </div>
                 {/* End main area */}
               </div>
             </div>
