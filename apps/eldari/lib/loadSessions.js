bb4bd9474f19aa940e93cc1376ea961422764a5b
Benjamin Bruington <Psychological_Chemist@hotmail.com>
2022-11-16 16:45:13 -0500

    https

--- apps/eldari/lib/loadSessions.js
+++ apps/eldari/lib/loadSessions.js
@@ -1,5 +1,5 @@
 export async function loadSessions() {
-      const res = await fetch(`${process.env.API_URL}/api/getSessions`, { method: 'GET',});
+      const res = await fetch(`https:${process.env.API_URL}/api/getSessions`, { method: 'GET',});
       if(res === undefined) return null;
       const data = await res.json()
       
