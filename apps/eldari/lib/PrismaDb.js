67f62f4bad5f39e3aa4cb14cb82dd09222388ba1
Benjamin Bruington <Psychological_Chemist@hotmail.com>
2022-11-17 11:43:16 -0500

    prismadb

--- apps/eldari/lib/PrismaDb.js
+++ apps/eldari/lib/PrismaDb.js
@@ -1,10 +1,3 @@
 import { PrismaClient } from '@prisma/client';
 export const prisma = new PrismaClient(); 
 
-export async function loadSessions() {
-      const res = await fetch(`/api/getSessions`, { method: 'GET',});
-      if(res === undefined) return null;
-      const data = await res.json()
-      
-      return data
-}
