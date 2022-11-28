3b2ab6d0d32fba5b2078cdc883f0be45ff867246
Benjamin Bruington <Psychological_Chemist@hotmail.com>
2022-11-28 16:29:21 -0500

    sign up form

--- apps/eldari/components/signUpForm.tsx
+++ apps/eldari/components/signUpForm.tsx
@@ -9,7 +9,7 @@ export default function SignUpForm() {
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               alt="Your Company"
             />
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign up</h2>
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign Up With a New Account</h2>
             <p className="mt-2 text-center text-sm text-gray-600">
               Or{' '}
               <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
