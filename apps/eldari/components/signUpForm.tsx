d595243488269517d5adaf6bd69803ae0ecc0003
Benjamin Bruington <Psychological_Chemist@hotmail.com>
2022-12-01 15:33:02 -0500

    form

--- apps/eldari/components/signUpForm.tsx
+++ apps/eldari/components/signUpForm.tsx
@@ -1,7 +1,7 @@
 export default function SignUpForm() {
   return (
     <>        
-      <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-14 ">
+      <div className="flex min-h-full flex-col mt-11 justify-center py-12 sm:px-6 lg:px-14 ">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <img
             className="mx-auto h-12 w-auto"
@@ -9,12 +9,7 @@ export default function SignUpForm() {
             alt="Your Company"
           />
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign Up</h2>
-          {/* <p className="mt-2 text-center text-sm text-gray-600">
-            Or{' '}
-            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-              start your 14-day free trial
-            </a>
-          </p> */}
+          
         </div>
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md ">
