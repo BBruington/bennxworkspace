063168ceab713564def1f29e5a31b38567c50c16
Benjamin Bruington <Psychological_Chemist@hotmail.com>
2022-12-06 15:20:48 -0500

    notes

--- apps/eldari/components/notes/noteSideBar.tsx
+++ apps/eldari/components/notes/noteSideBar.tsx
@@ -4,7 +4,14 @@ export default function NoteSideBar() {
 
   return (
     <>
-    
+      <div className="w-30% h-100vh border-r-gray-200">
+        <div className="flex space-between">
+          <h1>Notes</h1>
+
+          <button>Add</button>
+        </div>
+        <div className=""></div>
+      </div>
     </>
   )
 }
