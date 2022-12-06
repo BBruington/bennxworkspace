0faac4c84a2ea8db26f86a9b899d1ba2cf97901c
Benjamin Bruington <Psychological_Chemist@hotmail.com>
2022-12-06 17:33:21 -0500

    notes main

--- apps/eldari/components/notes/noteMain.tsx
+++ apps/eldari/components/notes/noteMain.tsx
@@ -4,7 +4,23 @@ export default function NoteMain() {
 
   return (
     <>
-      <div className="w-4/6 border-b-2">main</div>
+      <div className="w-4/6 h-90v border-b-2 app-main">
+        <div className="app-main-note-edit h-50v m-4">
+          
+          <input className="flex block mb-4 p-2 w-full h-8" type="text" placeholder="title" id="title" autoFocus />
+
+          <textarea className="flex block mb-4 p-2 w-full h-40v" id="body" placeholder="write your note here"/>
+
+        </div>
+
+        <div className="app-main-note-preview">
+
+          <h1 className="preview-title p-4 m-0">TITLE</h1>
+
+          <div className="markdown-preview p-4">note preview</div>
+
+        </div>
+      </div>
     </>
   )
 }
