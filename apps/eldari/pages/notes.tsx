9eeafc14fee4611e1be4a122023fc69b85ca044c
Benjamin Bruington <Psychological_Chemist@hotmail.com>
2022-12-06 15:06:13 -0500

    notes

---
+++ apps/eldari/pages/notes.tsx
@@ -0,0 +1,12 @@
+import NoteSideBar from '../components/notes/noteSideBar';
+import NoteMain from '../components/notes/noteMain';
+
+export default function Notes() {
+
+  return (
+    <>
+      <NoteSideBar />
+      <NoteMain />
+    </>
+  )
+}
