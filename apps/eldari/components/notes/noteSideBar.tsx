b17a34b60a956fc32ced99bd9b886136f254c69c
Benjamin Bruington <Psychological_Chemist@hotmail.com>
2022-12-06 22:29:47 -0500

    new note uuid

--- apps/eldari/components/notes/noteSideBar.tsx
+++ apps/eldari/components/notes/noteSideBar.tsx
@@ -1,6 +1,6 @@
 
 
-export default function NoteSideBar({notes}) {
+export default function NoteSideBar({notes, addNote}) {
 
   return (
     <>
@@ -8,18 +8,20 @@ export default function NoteSideBar({notes}) {
         <div className="flex justify-between p-4">
           <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-2xl lg:text-3xl m-0">Notes</h1>
 
-          <button className="text-teal-600 font-bold">Add</button>
+          <button onClick={addNote} className="text-teal-600 font-bold">Add</button>
         </div>
         <div className="app-sidebar-notes">
-          <div className="p-4 cursor-pointer hover:bg-gray-200 focus:bg-teal-300">
-            <div className="flex justify-between">
-              <strong>TITLE</strong>
-              <button className="text-orange-700 font-bold">Delete</button>
-            </div>
-            <p className="font-bold">Note preview</p>
+          {notes.map((note) => {
+            <div className="p-4 cursor-pointer hover:bg-gray-200 focus:bg-teal-300">
+              <div className="flex justify-between">
+                <strong>TITLE</strong>
+                <button className="text-orange-700 font-bold">Delete</button>
+              </div>
+              <p className="font-bold">Note preview</p>
 
-            <small className="font-light block">last modified [date]</small>
-          </div>
+              <small className="font-light block">last modified [date]</small>
+            </div>
+          })}
         </div>
 
       </div>
--- apps/eldari/pages/notes.tsx
+++ apps/eldari/pages/notes.tsx
@@ -1,14 +1,25 @@
 import NoteSideBar from '../components/notes/noteSideBar';
 import NoteMain from '../components/notes/noteMain';
+import uuid from 'react-uuid';
 import { useState } from 'react';
 
 export default function Notes() {
   const [notes, setNotes] = useState([])
 
+  const addNote = () => {
+
+    const newNote = {
+      id: uuid,
+      title:
+      body:
+      lastModified:
+    }
+  }
+
   return (
     <>
       <div className="flex justify-start">
-        <NoteSideBar notes={notes}/>
+        <NoteSideBar notes={notes} addNote={addNote}/>
         <NoteMain />
       </div>
     </>
