ddadbca4cb6b7297b9685f99b579ebff6f58fde4
Benjamin Bruington <Psychological_Chemist@hotmail.com>
2022-12-22 12:31:25 -0500

    nav bar

--- apps/eldari/components/navbar.jsx
+++ apps/eldari/components/navbar.jsx
@@ -24,13 +24,6 @@ const NavBar = () => {
     handleGetUser()
   },[router])
 
-  const pageLinks = [
-    {name: 'Locations'},
-    {name: 'Deities'},
-    {name: 'Sessions'},
-    {name: 'NPCS'},
-  ]
-
   const handleLogOut = async () => {
     await signOutUser();
     setUsers({})
@@ -45,17 +38,88 @@ const NavBar = () => {
     <div className="shadow bg-white py-2">
       <div className="h-17 my-3 pb-3 mx-auto px-5 flex items-center justify-between">
           <a className="text-2xl  transition-colors cursor-pointer" onClick={handleOnClickHome}>
-            <Image src='/icons/favicon_io/favicon-32x32.png' height={32} width={32} alt='github icon'/>
+            <Image src='/icons/favicon_io/favicon-32x32.png' height={32} width={32} alt='eldari temporary icon'/>
           </a>
           
-          <ul className="flex items-center gap-5">
-          {pageLinks.map((links) => ( 
+          <div className="flex items-center gap-5">
+            <Menu as="div" className="relative px-2 inline-block text-left ml-3 ">
+                <div>
+                  <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+                    Eldari Lore
+                    <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                  </Menu.Button>
+                </div>
+
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 z-10 mt-2  w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    {/* <div className="px-4 py-3">
+                      <p className="text-sm">Signed in as</p>
+                      <p className="truncate text-sm font-medium text-gray-900">{users.email}</p>
+                    </div> */}
+                    <div className="py-1 ">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="locations"
+                            className={classNames(
+                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                              'block px-4 py-2 text-sm'
+                            )}
+                          >
+                            Locations
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </div>
+                    <div className="py-1">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="npcs"
+                            className={classNames(
+                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                              'block px-4 py-2 text-sm'
+                            )}
+                          >
+                            NPCS
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </div>
+                    <div className="py-1">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="deities"
+                            className={classNames(
+                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                              'block px-4 py-2 text-sm'
+                            )}
+                          >
+                            Deities
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </div>
+                    
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+          {/* {pageLinks.map((links) => ( 
             <li key={links.name}>
               <Link href={`/${links.name.toLowerCase()}`} className="ml-2 hover:text-teal-400 transition-colors cursor-pointer">
                 {links.name}
               </Link>
             </li>
-          ))}
+          ))} */}
           { users.email? (
             <></>
           )
@@ -72,7 +136,7 @@ const NavBar = () => {
               <Menu as="div" className="relative inline-block text-left ml-3 ">
                 <div>
                   <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
-                    Options
+                    My Profile
                     <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
                   </Menu.Button>
                 </div>
@@ -92,32 +156,6 @@ const NavBar = () => {
                       <p className="truncate text-sm font-medium text-gray-900">{users.email}</p>
                     </div>
                     <div className="py-1">
-                      {/* <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                              'block px-4 py-2 text-sm'
-                            )}
-                          >
-                            Account settings
-                          </a>
-                        )}
-                      </Menu.Item> */}
-                      {/* <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                              'block px-4 py-2 text-sm'
-                            )}
-                          >
-                            Support
-                          </a>
-                        )}
-                      </Menu.Item> */}
                       <Menu.Item>
                         {({ active }) => (
                           <a
@@ -128,6 +166,21 @@ const NavBar = () => {
                             )}
                           >
                             My Notes
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </div>
+                    <div className="py-1">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="sessions"
+                            className={classNames(
+                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                              'block px-4 py-2 text-sm'
+                            )}
+                          >
+                            My Sessions
                           </a>
                         )}
                       </Menu.Item>
@@ -156,7 +209,7 @@ const NavBar = () => {
              </>
             )
           }
-          </ul>
+          </div>
       </div>
     </div>
   )
