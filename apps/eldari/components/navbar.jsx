203181355c689692b7caf58f018d392a02499bac
Benjamin Bruington <Psychological_Chemist@hotmail.com>
2022-12-22 12:38:41 -0500

    nav

--- apps/eldari/components/navbar.jsx
+++ apps/eldari/components/navbar.jsx
@@ -67,15 +67,15 @@ const NavBar = () => {
                     <div className="py-1 ">
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="locations"
+                          <Link
+                            href='locations'
                             className={classNames(
                               active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                               'block px-4 py-2 text-sm'
                             )}
                           >
                             Locations
-                          </a>
+                          </Link>
                         )}
                       </Menu.Item>
                     </div>
