e5d1e385e8def9f674f570b32842bc008ffe30b2
Benjamin Bruington <Psychological_Chemist@hotmail.com>
2022-12-27 20:46:13 -0500

    cleanup

--- apps/eldari/pages/test.tsx
+++ apps/eldari/pages/test.tsx
@@ -1,24 +1,9 @@
 import { useEffect, useState } from "react";
 import { notesCollectionRef} from "../../../libs/firebase/firebase";
-import { getDocs,  } from "firebase/firestore";
-import {ref, set, getDatabase} from "firebase/database"
+
 export default function Test() {
   async function writeUserData() {
-    const db = getDatabase()
-    // const data = await getDocs(notesCollectionRef)   
-    // const totalNotesData = data.docs.map((doc) => ({...doc.data(), id: doc.id}))
-    set(ref(db, 'user notes' + "someid"), {
-      email: "bibruington@gmail.com",
-      notes: [
-      {
-        body:"this is the note",
-        id:"cool new id",
-        lastModified:Date.now(),
-        title:"this is a title"
-      }
-    ]
-    });
-    console.log("CLICK")
+    console.log("write")
   }
 
   return (
